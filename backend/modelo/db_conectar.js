3417e4188f6049e5e5f7c6fd3336f0d982fc0ad7
valentinagonzalez <valentinagonsalez1240@gmail.com>
2026-04-23 01:23:56 -0500

    1:23

--- backend/modelo/db_conectar.js
+++ backend/modelo/db_conectar.js
@@ -8,13 +8,12 @@ let connection;
 export const getConnection = async () => {
   if (!connection) {
     connection = await mysql.createConnection({
-  host: "shinkansen.proxy.rlwy.net",
-  user: "root",
-  password: process.env.MYSQLPASSWORD,
-  database: "railway",
-  port: 20902
-});
+      host:     process.env.MYSQLHOST,
+      user:     process.env.MYSQLUSER,
+      password: process.env.MYSQLPASSWORD,
+      database: process.env.MYSQLDATABASE,
+      port:     Number(process.env.MYSQLPORT)
+    });
   }
-
   return connection;
 };
